6ba5d402c1d4284d9c4fd976cd68269bde5293ff
Jawad Tech <jawadbahixd@gmail.com>
2024-10-22 22:20:09 +0500

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -7,11 +7,11 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('⚡');
+    await m.React('♻️');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `> *KHAN-MD-SPEED: ${responseTime.toFixed(2)} s_*`;
+    const text = `> *KHAN-MD-SPEED: ${responseTime.toFixed(2)} s_⚡*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
